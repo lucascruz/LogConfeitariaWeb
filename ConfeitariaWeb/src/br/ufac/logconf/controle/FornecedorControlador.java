f995374de36b88fbe795eaca15276bd865ca5b61
loa <loa@loa-pc>
2018-12-15 14:09:20 -0500

    alteração e criação funcionario e fornecedor

---
+++ ConfeitariaWeb/src/br/ufac/logconf/controle/FornecedorControlador.java
@@ -0,0 +1,73 @@
+package br.ufac.logconf.controle;
+
+import java.util.*;
+import javax.faces.bean.*;
+
+import br.ufac.logconf.entidades.*;
+import br.ufac.logconf.repositorios.*;
+
+
+@ManagedBean(name="fornecedorControlador")
+@SessionScoped
+public class FornecedorControlador {
+
+	private List<Fornecedor> fornecedores;
+	private FornecedorRepositorio fo;
+	private CategoriaRepositorio cr;
+	private Fornecedor fornecedor;
+	private String chaveNome="";
+	
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public FornecedorControlador() {
+		fo = new FornecedorRepositorio();
+		cr = new CategoriaRepositorio();
+		
+	}
+
+	public List<Fornecedor> getFornecedores() {
+		fornecedores = fo.recuperarTodos();
+		return fornecedores;
+	}
+	
+
+	public String getChaveNome() {
+		return chaveNome;
+	}
+
+	public void setChaveNome(String chaveNome) {
+		this.chaveNome = chaveNome;
+	}
+
+	public String incluir() {
+		fornecedor = new Fornecedor();
+		return "funcionarioInclusao";
+	}
+	
+	public String adicionar() {
+	fo.adicionar(fornecedor);
+		return "funcionarioListagem";
+	}
+	
+	public String editar(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+		return "funcionarioEdicao";
+	}
+	
+	public String atualizar() {
+		fo.atualizar(fornecedor);
+		return "funcionarioListagem";
+	}
+	
+	public String excluir(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+		return "funcionarioExclusao";
+	}
+	
+	public String remover() {
+		fo.remover(fornecedor);
+		return "funcionarioListagem";
+	}
+}
--- ConfeitariaWeb/src/br/ufac/logconf/controle/FuncionarioControlador.java
+++ ConfeitariaWeb/src/br/ufac/logconf/controle/FuncionarioControlador.java
@@ -5,7 +5,7 @@ import javax.faces.bean.*;
 
 import br.ufac.logconf.entidades.Funcionario;
 import br.ufac.logconf.repositorios.FuncionarioRepositorio;
-import br.ufac.logconf.repositorios.PedidoRepositorio;
+
 
 @ManagedBean(name="funcionarioControlador")
 @SessionScoped
@@ -13,77 +13,26 @@ public class FuncionarioControlador {
 
 	private List<Funcionario> funcionarios;
 	private FuncionarioRepositorio fr;
-	private PedidoRepositorio pr;
 	private Funcionario funcionario;
-	private int funcionarioCodigo;
 	private String chaveNome="";
-	private String cpf;
-	private String endereco;
-	private int idade;
-	private String nome;
-	private String sexo;
+	
 	
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
 
-	public int getFuncionarioId() {
-		return funcionarioCodigo;
-	}
-
-	public void setFuncionarioCodigo(int funcCodigo) {
-		this.funcionarioCodigo = funcCodigo;
-	}
+	
 
 	public FuncionarioControlador() {
 		fr = new FuncionarioRepositorio();
-		pr = new PedidoRepositorio();
+	//	pr = new PedidoRepositorio();
 	}
 
 	public List<Funcionario> getFuncionarios() {
-	//	funcionarios = fr.recuperarTodosPorID(chaveNome);
+		funcionarios = fr.recuperarTodosPorNome();
 		return funcionarios;
 	}
 	
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public int getIdade() {
-		return idade;
-	}
-
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSexo() {
-		return sexo;
-	}
-
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
 
 	public String getChaveNome() {
 		return chaveNome;
@@ -99,30 +48,22 @@ public class FuncionarioControlador {
 	}
 	
 	public String adicionar() {
-		funcionario.setCpf(cpf);
-		funcionario.setEndereco(endereco);
-		funcionario.setIdade(idade);
-		funcionario.setNome(nome);
-		funcionario.setSexo(sexo);
-		fr.adicionar(funcionario);
+	fr.adicionar(funcionario);
 		return "funcionarioListagem";
 	}
 	
 	public String editar(Funcionario funcionario) {
 		this.funcionario = funcionario;
-		//cursoCodigo = aluno.getCurso().getCodigo();
 		return "funcionarioEdicao";
 	}
 	
 	public String atualizar() {
-		//funcionario.setCurso(cr.recuperar(cursoCodigo));
 		fr.atualizar(funcionario);
 		return "funcionarioListagem";
 	}
 	
-	public String excluir(Funcionario aluno) {
+	public String excluir(Funcionario funcionario) {
 		this.funcionario = funcionario;
-		//cursoCodigo = aluno.getCurso().getCodigo();
 		return "funcionarioExclusao";
 	}
 	
